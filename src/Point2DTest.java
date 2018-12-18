b9b056b1b065d316d71ab0a2f09f9519849fb847
chris250299 <tradang.isvnu@gmail.com>
2018-12-18 15:59:30 +0700

    point

---
+++ src/Point2DTest.java
@@ -0,0 +1,8 @@
+public class Point2DTest{
+    public static void main(String[] args) {
+        Point2D test = new Point2D();
+        Point2D test1 = new Point2D(3,5);
+        System.out.println(test);
+        System.out.println(test1);
+    }
+}
---
+++ src/Point3DTest.java
@@ -0,0 +1,9 @@
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D test2 = new Point3D();
+        Point3D test3 = new Point3D(3, 4, 5);
+        System.out.println(test2);
+        System.out.println(test3);
+
+    }
+}
